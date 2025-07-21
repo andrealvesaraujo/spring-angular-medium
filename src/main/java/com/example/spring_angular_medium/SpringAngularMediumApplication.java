6c28e9e84614982b09ec441a0d608acc4f43b4a4
andrealvesaraujo <andrealvesaraujo.1995@gmail.com>
2025-07-21 17:51:08 -0300

    feat: start project

---
+++ src/main/java/com/example/spring_angular_medium/SpringAngularMediumApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_angular_medium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularMediumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAngularMediumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_angular_medium/SpringAngularMediumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_angular_medium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAngularMediumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
