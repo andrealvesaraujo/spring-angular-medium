fcad2937c1d2ef28fc9085648912ffb522ef8dd4
andrealvesaraujo <andrealvesaraujo.1995@gmail.com>
2025-07-21 18:53:17 -0300

    fix: name of get product by id method in service in angular

--- frontend/angular-frontend/src/app/services/product.service.ts
+++ frontend/angular-frontend/src/app/services/product.service.ts
@@ -4,34 +4,30 @@ import { Observable } from 'rxjs';
 import { Product } from '../models/product.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-
   private apiUrl = 'http://localhost:8080/api/products';
 
   constructor(private http: HttpClient) {}
 
   getAllProducts(): Observable<Product[]> {
-      return this.http.get<Product[]>(this.apiUrl)
+    return this.http.get<Product[]>(this.apiUrl);
   }
 
-  getProduct(id: number): Observable<Product> {
-      return this.http.get<Product>(`${this.apiUrl}/${id}`)
+  getProductById(id: number): Observable<Product> {
+    return this.http.get<Product>(`${this.apiUrl}/${id}`);
   }
 
   createProduct(product: Product): Observable<Product> {
-      return this.http.post<Product>(this.apiUrl, product)
+    return this.http.post<Product>(this.apiUrl, product);
   }
 
   updateProduct(id: number, product: Product): Observable<Product> {
-      return this.http.put<Product>(`${this.apiUrl}/${id}`, product)
+    return this.http.put<Product>(`${this.apiUrl}/${id}`, product);
   }
 
   deleteProduct(id: number): Observable<void> {
-      return this.http.delete<void>(`${this.apiUrl}/${id}`)
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
   }
-
-
-
 }
