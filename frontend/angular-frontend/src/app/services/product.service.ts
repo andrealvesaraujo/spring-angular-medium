4e6b657ffadf429a2b292b8f4e6161889f690eac
andrealvesaraujo <andrealvesaraujo.1995@gmail.com>
2025-07-21 18:51:56 -0300

    feat: add methods of CRUD in product service

--- frontend/angular-frontend/src/app/services/product.service.ts
+++ frontend/angular-frontend/src/app/services/product.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../models/product.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:8080/api/products';
+
+  constructor(private http: HttpClient) {}
+
+  getAllProducts(): Observable<Product[]> {
+      return this.http.get<Product[]>(this.apiUrl)
+  }
+
+  getProduct(id: number): Observable<Product> {
+      return this.http.get<Product>(`${this.apiUrl}/${id}`)
+  }
+
+  createProduct(product: Product): Observable<Product> {
+      return this.http.post<Product>(this.apiUrl, product)
+  }
+
+  updateProduct(id: number, product: Product): Observable<Product> {
+      return this.http.put<Product>(`${this.apiUrl}/${id}`, product)
+  }
+
+  deleteProduct(id: number): Observable<void> {
+      return this.http.delete<void>(`${this.apiUrl}/${id}`)
+  }
+
+
+
 }
