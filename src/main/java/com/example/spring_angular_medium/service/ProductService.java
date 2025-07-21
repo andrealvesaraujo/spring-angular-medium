e3edb661964cdb737bd4c4fb1b8e837c7244e669
andrealvesaraujo <andrealvesaraujo.1995@gmail.com>
2025-07-21 18:06:59 -0300

    feat: add product service

---
+++ src/main/java/com/example/spring_angular_medium/service/ProductService.java
@@ -0,0 +1,42 @@
+package com.example.spring_angular_medium.service;
+
+import com.example.spring_angular_medium.model.Product;
+import com.example.spring_angular_medium.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public List<Product> getAllProducts(){
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(Long id){
+        return productRepository.findById(id).orElseThrow(() -> new RuntimeException("Product not found"));
+    }
+
+    public void createProduct(Product product){
+        productRepository.save(product);
+    }
+
+    public void updateProduct(Long id, Product newProduct){
+        Product product = getProductById(id);
+
+        product.setName(newProduct.getName());
+        product.setPrice(newProduct.getPrice());
+        product.setDescription(newProduct.getDescription());
+
+        productRepository.save(product);
+    }
+
+    public void deleteProduct(Long id){
+        productRepository.deleteById(id);
+    }
+
+}
